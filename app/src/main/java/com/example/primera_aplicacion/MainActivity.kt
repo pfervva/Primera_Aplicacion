a76b6053098e68cba3a0af2a27a725f005865150
Pedro from Macbook <pfernandezgonzalez@vva.safanet.es>
2023-10-27 14:01:41 +0200

    Version 0.3 - Codigo para abrir una URL implementado

--- app/src/main/java/com/example/primera_aplicacion/MainActivity.kt
+++ app/src/main/java/com/example/primera_aplicacion/MainActivity.kt
@@ -1,19 +1,34 @@
 package com.example.primera_aplicacion
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.widget.ImageButton
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val botonLlamada = findViewById<ImageButton>(R.id.boton_llamada)
+        val botonWeb = findViewById<ImageButton>(R.id.web)
 
         botonLlamada.setOnClickListener {
-            val intent = Intent(this, LlamadaActivity::class.java)
-            startActivity(intent)
+            val llamadaIntent = Intent(this, LlamadaActivity::class.java)
+            startActivity(llamadaIntent)
+        }
+
+        botonWeb.setOnClickListener {
+            val url = "https://www.google.com"
+            val webIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            startActivity(webIntent)
         }
     }
 }
+
+
+
+
