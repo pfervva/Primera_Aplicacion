4afd7cf4ff3bdb571a5382165543f723b0881fd9
Pedro from Macbook <pfernandezgonzalez@vva.safanet.es>
2023-10-27 18:21:35 +0200

    Version 0.4 -Codigo para poner una alarma implementado

--- app/src/main/java/com/example/primera_aplicacion/MainActivity.kt
+++ app/src/main/java/com/example/primera_aplicacion/MainActivity.kt
@@ -3,11 +3,9 @@ package com.example.primera_aplicacion
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.util.Log
+import android.provider.AlarmClock
 import android.widget.ImageButton
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,7 +13,7 @@ class MainActivity : AppCompatActivity() {
 
         val botonLlamada = findViewById<ImageButton>(R.id.boton_llamada)
         val botonWeb = findViewById<ImageButton>(R.id.web)
-
+        val botonAlarma = findViewById<ImageButton>(R.id.alarma)
         botonLlamada.setOnClickListener {
             val llamadaIntent = Intent(this, LlamadaActivity::class.java)
             startActivity(llamadaIntent)
@@ -26,7 +24,22 @@ class MainActivity : AppCompatActivity() {
             val webIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
             startActivity(webIntent)
         }
+        botonAlarma.setOnClickListener{
+            val currentTime = android.icu.util.Calendar.getInstance()
+            currentTime.add(android.icu.util.Calendar.MINUTE, 2)
+
+            val intent = Intent(AlarmClock.ACTION_SET_ALARM).apply {
+                putExtra(AlarmClock.EXTRA_MESSAGE, "Alarma")
+                putExtra(AlarmClock.EXTRA_HOUR, currentTime.get(android.icu.util.Calendar.HOUR_OF_DAY))
+                putExtra(AlarmClock.EXTRA_MINUTES, currentTime.get(android.icu.util.Calendar.MINUTE))
+            }
+            if (intent.resolveActivity(packageManager) != null) {
+                startActivity(intent)
+            }
+        }
+
     }
+
 }
 
 
