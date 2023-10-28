65cda01c8acb4e1f38bd83a0bbe50b72ea77fad7
Pedro from Macbook <pfernandezgonzalez@vva.safanet.es>
2023-10-28 11:21:23 +0200

    Version 0.5 - Codigo para hacer una captura implementado

--- app/src/main/java/com/example/primera_aplicacion/MainActivity.kt
+++ app/src/main/java/com/example/primera_aplicacion/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.primera_aplicacion
 
 import android.content.Intent
+import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
 import android.provider.AlarmClock
 import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+
 class MainActivity : AppCompatActivity() {
+    private lateinit var imageView: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -14,6 +20,9 @@ class MainActivity : AppCompatActivity() {
         val botonLlamada = findViewById<ImageButton>(R.id.boton_llamada)
         val botonWeb = findViewById<ImageButton>(R.id.web)
         val botonAlarma = findViewById<ImageButton>(R.id.alarma)
+        val botonCaptura = findViewById<ImageButton>(R.id.captura)
+        imageView = findViewById(R.id.imageView)
+
         botonLlamada.setOnClickListener {
             val llamadaIntent = Intent(this, LlamadaActivity::class.java)
             startActivity(llamadaIntent)
@@ -24,7 +33,8 @@ class MainActivity : AppCompatActivity() {
             val webIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
             startActivity(webIntent)
         }
-        botonAlarma.setOnClickListener{
+
+        botonAlarma.setOnClickListener {
             val currentTime = android.icu.util.Calendar.getInstance()
             currentTime.add(android.icu.util.Calendar.MINUTE, 2)
 
@@ -37,11 +47,22 @@ class MainActivity : AppCompatActivity() {
                 startActivity(intent)
             }
         }
-
+        botonCaptura.setOnClickListener {
+            val captura = captureScreen()
+            if (captura != null) {
+                imageView.setImageBitmap(captura)
+                Toast.makeText(this, "Captura pantalla hecha", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "No he podido tomar la captura de pantalla", Toast.LENGTH_LONG).show()
+            }
+        }
     }
 
+    private fun captureScreen(): Bitmap? {
+        val vista = window.decorView.rootView
+        vista.isDrawingCacheEnabled = true
+        val captura = Bitmap.createBitmap(vista.drawingCache)
+        vista.isDrawingCacheEnabled = false
+        return captura
+    }
 }
-
-
-
-
